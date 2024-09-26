cac296698e917504cc07fea6acab8b72a11a96f8
Aryan N. Jain <aryanj1084@gmail.com>
2024-09-26 23:31:36 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -277,7 +277,7 @@ const publicUrls = {
   socialProfiles: {
     resume: {
       title: "Resume",
-      link: "https://www.drive.google.com",
+      link: "https://drive.google.com/file/d/1mCR56TfGhRLG-_9UMqp96Ihaxq5L-1Lk/view?usp=sharing",
       icon: resume,
     },
     linkedin: {
